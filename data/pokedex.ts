b67c62d33fc7eb6f72b3a7098d7dcb03ec721338
ocleve713 <ocleve713@gmail.com>
2025-08-26 17:32:39 -0400

    Test pokedex.ts edit

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -1,10 +1,23 @@
 export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
-	bulbasaur: {
+	/**bulbasaur: {
 		num: 1,
 		name: "Bulbasaur",
 		types: ["Grass", "Poison"],
 		genderRatio: { M: 0.875, F: 0.125 },
 		baseStats: { hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45 },
+		abilities: { 0: "Overgrow", H: "Chlorophyll" },
+		heightm: 0.7,
+		weightkg: 6.9,
+		color: "Green",
+		evos: ["Ivysaur"],
+		eggGroups: ["Monster", "Grass"],
+	}*/
+	bulbasaur: {
+		num: 1,
+		name: "Bulbasaur",
+		types: ["Electric", "Fairy"],
+		genderRatio: { M: 0.875, F: 0.125 },
+		baseStats: { hp: 69, atk: 69, def: 69, spa: 69, spd: 69, spe: 69 },
 		abilities: { 0: "Overgrow", H: "Chlorophyll" },
 		heightm: 0.7,
 		weightkg: 6.9,
